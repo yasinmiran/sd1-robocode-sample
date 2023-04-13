6f6d6008062fd859d131d2f9e460de49ac5916af
Yasin <wytm97@protonmail.com>
2023-04-13 15:54:07 +0100

    feat: add dependency and custom robot

--- src/main/java/dev/yasint/Main.java
+++ src/main/java/dev/yasint/Main.java
@@ -3,7 +3,7 @@ package dev.yasint;
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 
 }
---
+++ src/main/java/dev/yasint/MyFirstRobot.java
@@ -0,0 +1,9 @@
+package dev.yasint;
+
+import robocode.Robot;
+
+public class MyFirstRobot extends Robot {
+    
+    // Your custom implementation goes here
+
+}
